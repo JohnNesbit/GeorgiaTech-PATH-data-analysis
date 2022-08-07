47b325b51082881d167ca0743e72f5cc5d84d918
John Nesbit <49009243+JohnNesbit@users.noreply.github.com>
2022-08-07 18:33:38 -0400

    revamp strategy

--- DataCleaner.py
+++ DataCleaner.py
@@ -5,57 +5,30 @@ import os
 # import csvs into pandas dataframes, combine them
 path = "ICPSR_36498-V16 (1)/ICPSR_36498" # put path to list of "DS" directories of "deliniated" version
 
-nonSmokerDf = []
-wantedCols = ["R01_EVER_USER_CIGS"] # wanted columns of information
-foundWantedCols = []
-with open("ColumnInfo.txt", "r") as f:
-	for col in f.read().split(","):
-		wantedCols.append(col)
-
+# find tsv file in the directory given
 def findData(path):
 	for file in os.listdir(path):
 		if("-Data.tsv" in file):
 			return path + file
 
-_ = 0
-for i in os.listdir(path):
-	if("DS" not in i):
-		continue
-	print(findData(path+"/"+i+"/"))
-	df = pd.read_csv(findData(path+"/"+i+"/"), sep='\t')
-
-	if(_ == 0): # initialize dir
-		for wcol in wantedCols:
-			if (wcol in df.columns):
-				nonSmokerDf = df[["PERSONID", wcol]] # merge dataframes, they have new data on same people
-				foundWantedCols.append(wcol)
-	elif(len(nonSmokerDf) > 10): # add onto dir once initialized, for some reason I had to add the elif, dont ask
-		# get columns we want
-		for wcol in wantedCols:
-			if (wcol in df.columns):
-				nonSmokerDf.merge(df[["PERSONID", wcol]], on="PERSONID", how='left') # merge dataframes, they have new data on same people
-				if(wcol not in foundWantedCols):
-					foundWantedCols.append(wcol)
+waves = {}
+for i in range(1, 6): # loop through waves of questionarres
+	for j in range(1, 3): # j determines adult or youth questionarre file, Youth is 2
+		# this line loads the given wave and age group into the dict "waves" with a correspoding hash
+		waves[str(i) + "_" + str(j)] = pd.read_csv(findData("E:/PycharmProjects/Gt-data-science-project/ICPSR_36498-V16 (1)/ICPSR_36498/DS"+ str(i) +"00"+ str(j) +"/"), sep='\t', low_memory=False)
 
 
-	if(_ > 2): # if you want to test, only uses 3 dataframes so not to take 2 years to run and all 8 gigs of RAM
-		break
-	_ += 1
+# get people who were new smokers in the wave and get their previous wave data for prediction
+getSmokers = lambda wave, var, previousWave: previousWave[np.isin(previousWave["PERSONID"], wave[wave[var] == 1]["PERSONID"])]
 
+# get youth who have smoked since the first wave
+# include age-up adults by finding the new smokers and then only findiing matches within the last wave's youth files
+adict = {1:"A", 2:"Y"}
+for i in range(2, 6):
+	for j in range(1, 3):
+		if (i == 2 and j == 1):
+			smokers = getSmokers(waves["2_1"], "R02R_A_NEW_CIGS", waves["1_2"])
+			continue
+		smokers = smokers.append(getSmokers(waves[str(i) + "_" + str(j)], "R0"+ str(i) +"R_"+ adict[j] +"_NEW_CIGS", waves[str(i-1) + "_2"]), sort=True)
 
-# delete those who have smoked cigarettes before
-#import gc
-#nonSmokerDf = nonSmokerDf[nonSmokerDf["R04_EVER_NEVER_CIGS"] == 2] # 2 is never user - DS4001/36498-4001-Questionnaire.pdf pg 25
-#gc.collect()
-
-cols = nonSmokerDf.columns
-print("cols: ", len(cols))
-print("rows: ", len(nonSmokerDf))
-for i in cols: # print the cols we kept, ensure everything is there
-	print(i)
-
-for col in foundWantedCols:
-	nonSmokerDf.loc[nonSmokerDf[col] < 1, col] = 0 # get rid of negative answers and turn into 0
-
-# create csv and save filtered data"""
-nonSmokerDf.to_csv("Cleaned_data.csv")
+print(len(smokers)) # 1824 youths started smoking during this experiment
