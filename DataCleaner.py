227f8114e035eb97a1bcebe53c3d3f7760df9c2e
John Nesbit <49009243+JohnNesbit@users.noreply.github.com>
2022-08-08 18:17:04 -0400

    Get rid of NaNs, removes 6% of data as collateral

--- DataCleaner.py
+++ DataCleaner.py
@@ -32,3 +32,32 @@ for i in range(2, 6):
 		smokers = smokers.append(getSmokers(waves[str(i) + "_" + str(j)], "R0"+ str(i) +"R_"+ adict[j] +"_NEW_CIGS", waves[str(i-1) + "_2"]), sort=True)
 
 print(len(smokers)) # 1824 youths started smoking during this experiment
+
+for col in smokers.columns: # have to go through cols manually instead of np.where because NaNs must be excepted
+	if col == "PERSONID" or col == "CASEID":
+		continue
+
+	try:
+		smokers[col] = pd.to_numeric(smokers[col], downcast="integer")
+	except:
+		smokers = smokers.drop(columns=[col])
+		print(col)
+		continue
+
+	smokers[col] = np.where(np.nan_to_num(smokers[col]) < 0, np.nan, smokers[col])
+
+nanlist = [] # linked list for nans and columns
+for col in smokers.columns:
+	nanlist.append(smokers[col].isnull().sum()) # count nans in each column
+
+print(smokers.columns[np.argmax(np.array(nanlist))], ": ", max(nanlist)) # see how many nans are within highest var
+# was 1428/1824. clearly not a relevant or acceptable variable
+
+print(len(smokers.columns))
+colist = []
+for l in nanlist:
+	if l >= 1:
+		colist.append(smokers.columns[l])
+
+smokers = smokers.drop(columns=colist)
+print(len(smokers.columns))
