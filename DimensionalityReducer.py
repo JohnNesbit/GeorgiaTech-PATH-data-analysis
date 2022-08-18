95cf1769a28de27d122c4decfc6923219ee51871
John Nesbit <49009243+JohnNesbit@users.noreply.github.com>
2022-08-17 20:17:44 -0400

    Update DimensionalityReducer.py

--- DimensionalityReducer.py
+++ DimensionalityReducer.py
@@ -3,12 +3,15 @@ import numpy as np
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
 
-#load data and drop label data so there is no data contaimination
-data = pd.read_csv("Data.csv")
+data = pd.read_csv("DirtyData2000.csv")
+print(data.columns)
+
+for i in range(2, 6):
+	print(i, ": ", len(data[data["WAVE"] == i])/len(data))
+
 target = data["Target"]
 data = data.drop(columns=["PERSONID", "CASEID", "Target", "Unnamed: 0"])
 
-# final NaN check for stragglers
 for i in data.columns:
 	data[i] = np.nan_to_num(data[i])
 
@@ -19,19 +22,17 @@ scaling = StandardScaler()
 scaling.fit(data)
 Scaled_data = scaling.transform(data)
 
-# do PCA
-principal = PCA(n_components=60)
+
+principal = PCA(n_components=356)
 principal.fit(Scaled_data)
 x = principal.transform(Scaled_data)
 
 # Check the dimensions of data after PCA
 print(x.shape)
 
-# print variance retained
 print(sum(principal.explained_variance_ratio_)) # retain 88% of variance with halving variables
 
-# add target vars back in and save data
-data = pd.DataFrame(x, columns=[list(range(x.shape[1]))])
+data = pd.DataFrame(x, columns=[list(range(x.shape[1]))]) # ]
 data["Target"] = target
 print(len(data.columns))
-data.to_csv("Squeeze.csv")
+data.to_csv("DirtyDataSqueeze.csv")
