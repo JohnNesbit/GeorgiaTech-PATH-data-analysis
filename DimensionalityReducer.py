b5b55ab0b07b3ffd88d89e25ad4d47f2bb0c6d7c
John Nesbit <49009243+JohnNesbit@users.noreply.github.com>
2022-08-23 20:22:29 -0400

    One-hot encode categorical variables

--- DimensionalityReducer.py
+++ DimensionalityReducer.py
@@ -3,11 +3,7 @@ import numpy as np
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
 
-data = pd.read_csv("DirtyData2000.csv")
-print(data.columns)
-
-for i in range(2, 6):
-	print(i, ": ", len(data[data["WAVE"] == i])/len(data))
+data = pd.read_csv("Data3000.csv")
 
 target = data["Target"]
 data = data.drop(columns=["PERSONID", "CASEID", "Target", "Unnamed: 0"])
@@ -15,24 +11,37 @@ data = data.drop(columns=["PERSONID", "CASEID", "Target", "Unnamed: 0"])
 for i in data.columns:
 	data[i] = np.nan_to_num(data[i])
 
-# Scale data before applying PCA
+catData = []
+for i in data.columns:
+	if sum(sum([data[i] > 2.0])) == 0 and sum(sum([data[i] < 0.0])) == 0:
+		catData.append(i)
+print(len(catData))
+catData = data[catData].copy()
+data = data.drop(columns=catData)
+numpyCat = np.array([])
+
+for i in catData.columns:
+	numpyCat = np.append(numpyCat, np.array([catData[i] == 2]).astype(int))
+	numpyCat = np.append(numpyCat, np.array([catData[i] == 1]).astype(int))
+	numpyCat = numpyCat.reshape(numpyCat.size//len(catData[i]), len(catData[i]))
+print(numpyCat.shape)
+
+# Scale data
 scaling = StandardScaler()
 
 # Use fit and transform method
 scaling.fit(data)
 Scaled_data = scaling.transform(data)
 
-
-principal = PCA(n_components=356)
-principal.fit(Scaled_data)
-x = principal.transform(Scaled_data)
-
-# Check the dimensions of data after PCA
-print(x.shape)
-
-print(sum(principal.explained_variance_ratio_)) # retain 88% of variance with halving variables
-
-data = pd.DataFrame(x, columns=[list(range(x.shape[1]))]) # ]
+print(Scaled_data.shape)
+Scaled_data = Scaled_data.reshape([3310, 438])
+new = numpyCat #np.append(Scaled_data, numpyCat).reshape(3310, 4150740//3310)
+print(new.shape)
+cat = pd.DataFrame(numpyCat.reshape(3310, 816)) # ] # , columns=[list(range(len(new)))]
+data = pd.DataFrame(Scaled_data).join(cat, sort=False, how="left", lsuffix="l", rsuffix="r")
 data["Target"] = target
 print(len(data.columns))
-data.to_csv("DirtyDataSqueeze.csv")
+
+
+data.to_csv("Data3000Cat.csv")
+
